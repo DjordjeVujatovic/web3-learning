b12eef5bcd96ee2a340a580fae464816e892d0e5
Djordje Vujatovic <djordjevujatovic@Djordjes-MacBook-Pro.local>
2022-02-17 11:43:51 -0800

    Add color palate, constants

---
+++ src/settings/colorPalette.js
@@ -0,0 +1,10 @@
+const colors = {
+  base_onyx: "#3D3D3E",
+  base_purple: "#A8AEC1",
+  base_opal: "#B5D2CB",
+  base_apple_green: "#BFFFBC",
+  base_mint_green: "#A6FFA1",
+  base_yellow: "#F3CA40",
+};
+
+export { colors };
---
+++ src/settings/constants.js
@@ -0,0 +1,10 @@
+const SAMPLE_WALLET_ADDRESS = "0xEBfb4E44a5F12E371aAd48ca0263a1373BAd0361";
+const ETHEREUM_REQUES_ACCOUNTS_METHOD = "eth_requestAccounts";
+const RARIBLE_API_URL =
+  "https://api.rarible.org/v0.1/items/byOwner/?owner=ETHEREUM:";
+
+export {
+  SAMPLE_WALLET_ADDRESS,
+  ETHEREUM_REQUES_ACCOUNTS_METHOD,
+  RARIBLE_API_URL,
+};
